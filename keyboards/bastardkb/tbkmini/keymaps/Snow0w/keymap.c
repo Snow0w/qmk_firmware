78f58070c3ad2939d5f9916f9a97eae15835aa46
snow0w <kkiirriikm@gmail.com>
2025-04-20 12:59:00 +0300

    add windows layouts

--- keyboards/bastardkb/tbkmini/keymaps/Snow0w/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/Snow0w/keymap.c
@@ -17,11 +17,159 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    WIN_LEFT_BRACE = SAFE_RANGE,
+    WIN_RIGHT_BRACE,
+    WIN_LEFT_BRACKET,
+    WIN_RIGHT_BRACKET,
+    WIN_LEFT_TRIAG,
+    WIN_RIGHT_TRIAG,
+    WIN_SLASH,
+    WIN_BACK_SLASH,
+    WIN_SOBAKA,
+    WIN_RESHETKA,
+    WIN_DOLLAR,
+    WIN_CAP,
+    WIN_AND,
+    WIN_TILDE,
+    WIN_APOSTRO,
+    WIN_PIPE,
+    WIN_QUOTE,
+    WIN_DOUBLE_QUOTE,
+    WIN_SEMICOLON,
+    WIN_COLON,
+    WIN_DOT,
+    WIN_COMMA,
+    WIN_QUESTION,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case WIN_LEFT_BRACE:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_1); tap_code16(KC_KP_2); tap_code16(KC_KP_3); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_RIGHT_BRACE:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_1); tap_code16(KC_KP_2); tap_code16(KC_KP_5); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_LEFT_BRACKET:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_9); tap_code16(KC_KP_1); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_RIGHT_BRACKET:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_9); tap_code16(KC_KP_3); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_LEFT_TRIAG:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_6); tap_code16(KC_KP_0); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_RIGHT_TRIAG:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_6); tap_code16(KC_KP_2); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_SLASH:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_4); tap_code16(KC_KP_7); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_BACK_SLASH:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_9); tap_code16(KC_KP_2); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_SOBAKA:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_6); tap_code16(KC_KP_4); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_RESHETKA:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_3); tap_code16(KC_KP_5); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_DOLLAR:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_3); tap_code16(KC_KP_6); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_CAP:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_9); tap_code16(KC_KP_4); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_AND:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_3); tap_code16(KC_KP_8); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_TILDE:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_1); tap_code16(KC_KP_2); tap_code16(KC_KP_6); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_APOSTRO:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_9); tap_code16(KC_KP_6); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_PIPE:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_1); tap_code16(KC_KP_2); tap_code16(KC_KP_4); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_QUOTE:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_3); tap_code16(KC_KP_9); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_DOUBLE_QUOTE:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_3); tap_code16(KC_KP_4); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_SEMICOLON:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_5); tap_code16(KC_KP_9); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_COLON:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_5); tap_code16(KC_KP_8); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_DOT:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_4); tap_code16(KC_KP_6); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_COMMA:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_4); tap_code16(KC_KP_4); unregister_code(KC_LALT);
+            }
+            return false;
+        case WIN_QUESTION:
+            if (record->event.pressed) {
+                register_code(KC_LALT); tap_code16(KC_KP_6); tap_code16(KC_KP_3); unregister_code(KC_LALT);
+            }
+            return false;
+        default:
+            return true;
+    }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        XXXXXXX, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, XXXXXXX,
+        XXXXXXX, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         LGUI(KC_SPC), MT(MOD_RGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F), KC_G, KC_H, MT(MOD_LSFT, KC_J), MT(MOD_LCTL, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -35,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        XXXXXXX, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, XXXXXXX,
+        XXXXXXX, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BACKSLASH,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         XXXXXXX, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_KP_1, KC_KP_2, KC_BACKSLASH,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -47,7 +195,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, LGUI(KC_SPC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, LGUI(KC_SPC), LGUI(KC_SPC), XXXXXXX, XXXXXXX, XXXXXXX, KC_RIGHT_BRACKET,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         XXXXXXX, MT(MOD_RGUI, KC_F6), MT(MOD_LALT, KC_F7), MT(MOD_LCTL, KC_F8), MT(MOD_LSFT, KC_F9), KC_F10, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_LEFT_BRACKET, KC_RIGHT_BRACKET,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -58,6 +206,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
 
     [3] = LAYOUT_split_3x6_3(
+        //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NUM_LOCK,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        RM_NEXT, RM_HUED, RM_SATD, RM_VALD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+        XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+        //`--------------------------'  `--------------------------'
+        ),
+    [4] = LAYOUT_split_3x6_3(
+        //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        XXXXXXX, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        LGUI(KC_SPC), MT(MOD_RGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_F), KC_G, KC_H, MT(MOD_LSFT, KC_J), MT(MOD_LCTL, KC_K), MT(MOD_LALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        XXXXXXX, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, XXXXXXX,
+        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                              MT(MOD_LCTL, KC_TAB), LT(5, KC_ESC), LT(6, KC_SPC), KC_ENT, KC_BSPC, MT(MOD_LALT, KC_DEL)
+
+        //`--------------------------'  `--------------------------'
+
+        ),
+
+    [5] = LAYOUT_split_3x6_3(
+        //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        XXXXXXX, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BACKSLASH,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        XXXXXXX, KC_EXLM, WIN_SOBAKA, WIN_RESHETKA, WIN_DOLLAR, KC_PERC, WIN_CAP, WIN_AND, KC_ASTR, WIN_QUOTE, WIN_SEMICOLON, KC_BACKSLASH,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        XXXXXXX, WIN_TILDE, WIN_APOSTRO, KC_MINUS, KC_PLUS, KC_PLUS, WIN_PIPE, KC_EQUAL, KC_UNDERSCORE, WIN_DOUBLE_QUOTE, WIN_COLON, XXXXXXX,
+        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+        MT(MOD_LCTL, KC_TAB),  _______, MO(7), WIN_COMMA, WIN_DOT, WIN_QUESTION
+        //`--------------------------'  `--------------------------'
+        ),
+
+    [6] = LAYOUT_split_3x6_3(
+        //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, LGUI(KC_SPC), LGUI(KC_SPC), XXXXXXX, XXXXXXX, XXXXXXX, KC_RIGHT_BRACKET,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        XXXXXXX, MT(MOD_RGUI, KC_F6), MT(MOD_LALT, KC_F7), MT(MOD_LCTL, KC_F8), MT(MOD_LSFT, KC_F9), KC_F10, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_LEFT_BRACKET, KC_RIGHT_BRACKET,
+        //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        XXXXXXX, KC_F11, KC_F12, KC_LEFT_SHIFT, KC_DEL, KC_INS, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_GRV, KC_QUOTE, KC_QUOTE, XXXXXXX,
+        //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+        MT(MOD_LCTL, KC_TAB), MO(7), _______, KC_ENT, KC_BSPC, MT(MOD_LALT, KC_DEL)
+        //`--------------------------'  `--------------------------'
+        ),
+
+    [7] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -82,6 +279,7 @@ enum combos {
     MAC_RIGHT_PARENT,
     MAC_LEFT_BRACE,
     MAC_RIGHT_BRACE,
+    CHANGE_LAYOUT_COMBO,
 };
 
 const uint16_t PROGMEM mac_slash[] = {KC_COMM, KC_DOT, COMBO_END};
@@ -94,6 +292,7 @@ const uint16_t PROGMEM mac_left_parent[] = {MT(MOD_LCTL, KC_D), MT(MOD_LSFT, KC_
 const uint16_t PROGMEM mac_right_parent[] = {MT(MOD_LSFT, KC_J), MT(MOD_LCTL, KC_K), COMBO_END};
 const uint16_t PROGMEM mac_left_brace[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM mac_right_brace[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM change_layout_combo[] = {KC_R, KC_T, KC_Y, KC_U, COMBO_END};
 
 combo_t key_combos[] = {
     [MAC_SLASH] = COMBO(mac_slash, KC_NO),
@@ -106,7 +305,19 @@ combo_t key_combos[] = {
     [MAC_RIGHT_PARENT] = COMBO(mac_right_parent, KC_NO),
     [MAC_LEFT_BRACE] = COMBO(mac_left_brace, KC_NO),
     [MAC_RIGHT_BRACE] = COMBO(mac_right_brace, KC_NO),
+    [CHANGE_LAYOUT_COMBO] = COMBO(change_layout_combo, TG(4)),
 };
+
+void send_alt_code(uint8_t digits[], uint8_t len) {
+    register_code(KC_LALT);
+    wait_ms(5);
+    for (uint8_t i = 0; i < len; i++) {
+        register_code(digits[i]);
+        unregister_code(digits[i]);
+        wait_ms(5);
+    }
+    unregister_code(KC_LALT);
+}
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
     if (pressed) {
@@ -115,70 +326,78 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
                 if (layer_state_is(0)) {
                     tap_code16(KC_KP_8);
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    uint8_t code[] = {KC_KP_4, KC_KP_7};
+                    send_alt_code(code, 2);
                 }
                 break;
             case MAC_BACKSLASH:
                 if (layer_state_is(0)) {
                     tap_code16(LSFT(KC_KP_8));
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    uint8_t code[] = {KC_KP_9, KC_KP_2};
+                    send_alt_code(code, 2);
                 }
                 break;
             case MAC_LEFT_TRIAG:
                 if (layer_state_is(0)) {
                     tap_code16(KC_KP_7);
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    uint8_t code[] = {KC_KP_6, KC_KP_0};
+                    send_alt_code(code, 2);
                 }
                 break;
             case MAC_RIGHT_TRIAG:
                 if (layer_state_is(0)) {
                     tap_code16(LSFT(KC_KP_7));
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    uint8_t code[] = {KC_KP_6, KC_KP_2};
+                    send_alt_code(code, 2);
                 }
                 break;
             case MAC_LEFT_BRACKET:
                 if (layer_state_is(0)) {
                     tap_code16(KC_KP_5);
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    uint8_t code[] = {KC_KP_9, KC_KP_1};
+                    send_alt_code(code, 2);
                 }
                 break;
             case MAC_RIGHT_BRACKET:
                 if (layer_state_is(0)) {
                     tap_code16(LSFT(KC_KP_5));
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    uint8_t code[] = {KC_KP_9, KC_KP_3};
+                    send_alt_code(code, 2);
                 }
                 break;
             case MAC_LEFT_PARENT:
                 if (layer_state_is(0)) {
                     tap_code16(KC_LEFT_PAREN);
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    tap_code16(KC_LEFT_PAREN);
                 }
                 break;
             case MAC_RIGHT_PARENT:
                 if (layer_state_is(0)) {
                     tap_code16(KC_RIGHT_PAREN);
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    tap_code16(KC_RIGHT_PAREN);
                 }
                 break;
             case MAC_LEFT_BRACE:
                 if (layer_state_is(0)) {
                     tap_code16(KC_KP_6);
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    uint8_t code[] = {KC_KP_1, KC_KP_2, KC_KP_3};
+                    send_alt_code(code, 3);
                 }
                 break;
             case MAC_RIGHT_BRACE:
                 if (layer_state_is(0)) {
                     tap_code16(LSFT(KC_KP_6));
                 } else if (layer_state_is(4)) {
-                    tap_code16(KC_NO);
+                    uint8_t code[] = {KC_KP_1, KC_KP_2, KC_KP_5};
+                    send_alt_code(code, 3);
                 }
                 break;
         }
